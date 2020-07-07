9029f45bb496c2219afe22759ad9ca8d8b56fcd8
Spencer <sspencerotoni@gmail.com>
2020-07-06 21:50:24 -0300

    Commit

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,20 +1,81 @@
+import * as Yup from 'yup';
 import Recipient from '../models/recipient';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.string().required(),
+      complement: Yup.string(),
+      state: Yup.string().required().max(2),
+      city: Yup.string().required(),
+      cep: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const { name } = req.body;
     const existRecipient = await Recipient.findOne({ where: { name } });
 
     if (existRecipient) {
-      return res.status(400).json({ error: 'Recipient not found.' });
+      return res.status(400).json({ error: 'Recipient alredy exist.' });
     }
 
-    const recipient = await Recipient.create(req.body);
+    const {
+      id,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    } = await Recipient.create(req.body);
 
-    return res.json(recipient);
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    });
   }
 
-  async update(req, res) {}
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.string(),
+      complement: Yup.string(),
+      state: Yup.string().max(2),
+      city: Yup.string(),
+      cep: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { name } = req.body;
+
+    const recipient = await Recipient.findOne({ where: { name } });
+
+    if (!recipient) {
+      return res.status(401).json({ error: 'Recipient not found.' });
+    }
+
+    const { id } = await recipient.update(req.body);
+
+    return res.json({
+      id,
+      name,
+    });
+  }
 }
 
 export default new RecipientController();
--- src/app/controllers/UserControllers.js
+++ src/app/controllers/UserControllers.js
@@ -1,7 +1,18 @@
+import * as Yup from 'yup';
 import User from '../models/user';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required().email(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
     const { email } = req.body;
     const userExist = await User.findOne({ where: { email } });
 
@@ -18,9 +29,46 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
 
-    return res.json(true);
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+    });
   }
 }
 
